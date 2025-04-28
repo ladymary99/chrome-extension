d36cdfdbaef2ca2a657c085ea15d545acddfc555
ladymary99 <marycherie999@gmail.com>
2025-04-28 23:06:52 +0430

    added jason

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function () {
-  console.log("Button clicked!");
-});
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML = listItems  
+}
